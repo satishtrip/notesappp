08bd5caf1e00c2598688b60d51cd1b61d61fa6ab
satishtrip <satish.tripathi1297@gmail.com>
2020-12-08 03:08:02 +0530

    npm packages

--- notesapp/hello.js
+++ notesapp/hello.js
@@ -1,6 +1,13 @@
 appp = require('./nts.js')
-
+const chalk = require('chalk')
+const validator = require('validator')
 const prod = appp()
 
 console.log(prod)
 console.log("function has been called")
+console.log(validator.isEmail('satishl.com'))
+console.log(validator.isURL('https://nodejs.org/en/'))
+const redmsg = chalk.green.underline.bold('hey')
+console.log(redmsg)
+const msg = chalk.red.inverse.bold('brother')
+console.log(msg)
